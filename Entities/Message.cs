ee41c584be66f9d32b7c6231105740cbf327cbfb
BnnQ <1v25fa94@gmail.com>
2023-12-24 11:29:07 +0200

    Added entities

--- Entities/Message.cs
+++ Entities/Message.cs
@@ -4,7 +4,8 @@ public class Message
 {
     public int Id { get; set; }
     public string Text { get; set; } = default!;
-
+    public DateTime SentAt { get; set; }
+    
     public string SenderId { get; set; } = default!;
     public User Sender { get; set; } = default!;
 
--- Entities/User.cs
+++ Entities/User.cs
@@ -4,8 +4,8 @@ namespace MessengerServer.Entities;
 
 public class User : IdentityUser
 {
+    public string LastFcmToken { get; set; } = null!;
     public string AvatarImagePath { get; set; } = null!;
-
     public ICollection<Chat> Chats { get; set; } = new List<Chat>();
     public ICollection<Message> Messages { get; set; } = new List<Message>();
 }
